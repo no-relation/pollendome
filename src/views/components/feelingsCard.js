1e844970af4536f3e091d856d723fbfa1b3dfd4a
no-relation <eddiec76@gmail.com>
2019-01-18 17:00:14 -0600

    conditional render on feelings card

--- src/views/components/feelingsCard.js
+++ src/views/components/feelingsCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Icon } from 'semantic-ui-react';
+import { Card, Icon, List, Container } from 'semantic-ui-react';
 
 export class FeelingsCard extends Component {
 
@@ -13,18 +13,69 @@ export class FeelingsCard extends Component {
         return month + ' ' + numDay + ', ' + year
     }
 
+    getSporeList = (day) => {
+        let list = []
+        Object.keys(day).forEach(key => {
+            if (['id', 'month', 'date', 'year', 'fulldate', 'created_at', 'updated_at'].indexOf(key) === -1) {
+                list.push(key)
+            }
+        })
+        const limitedList = list.filter((item) => Number(day[item]) > 20)
+        const topFive = limitedList.sort((a, b) => b - a).slice(0, 5)
+        return (
+            topFive.map(spore => {
+                return <List.Item>{`${spore} (${day[spore]})`}</List.Item>
+            })
+        )
+    }
+
+    renderBadSporeList = () => {
+        const spores = this.getSporeList(this.props.feeling.day)
+        if(spores.length === 0) {
+            return (
+                <Card.Description>If you're allergic to something, it looks like it wasn't in the air that day.</Card.Description>
+            )
+        } else {
+            return (
+                <Container>
+                    <Card.Description>You might be allergic to:</Card.Description>
+                    <List bulleted>
+                        {spores}
+                    </List>
+                </Container>
+            )
+        }
+    }
+
+    renderNotBadSporeList = () => {
+        const spores = this.getSporeList(this.props.feeling.day)
+        if(spores.length === 0) {
+            return null
+        } else {
+            return (
+                <Container>
+                    <Card.Description>You might not be allergic to:</Card.Description>
+                    <List bulleted>
+                        {spores}
+                    </List>
+                </Container>
+            )
+        }
+    }
+
     render() {
         let isGoodFeeling
-        if (this.props.feeling[0].rating > 3) {
+        if (this.props.feeling.feeling.rating > 3) {
             isGoodFeeling = true
         } else {
             isGoodFeeling = false
         }
         return (
-            <Card color={isGoodFeeling ? "green" : "red"} >
+            <Card link color={isGoodFeeling ? "green" : "red"} >
                 <Card.Content>
-                    <Icon name={isGoodFeeling ? 'thumbs up outline' : 'thumbs down outline'} />
-                    <Card.Header>{this.formatMonthDayYear(this.props.feeling[1].fulldate)} </Card.Header>
+                    <Icon name={isGoodFeeling ? 'thumbs up outline' : 'thumbs down outline'} color={isGoodFeeling ? "green" : "red"}/>
+                    <Card.Header>{this.formatMonthDayYear(this.props.feeling.day.fulldate)} </Card.Header>
+                    {isGoodFeeling ? this.renderNotBadSporeList() : this.renderBadSporeList()}
                 </Card.Content>
             </Card>
         );
--- src/views/containers/feelingsContainer.js
+++ src/views/containers/feelingsContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Icon } from 'semantic-ui-react';
 import { FeelingsCard } from '../components/feelingsCard';
 import { FeelingsForm } from '../components/feelingsForm';
 
@@ -8,7 +8,13 @@ export class FeelingsContainer extends Component {
         if (this.props.feelings){
             return (
                 <Card.Group>
-                    {this.props.feelings.map(feeling => <FeelingsCard feeling={feeling} /> )}
+                    {this.props.feelings.map(feeling => <FeelingsCard key={feeling.feeling.id} feeling={feeling} /> )}
+                    {/* <Card link color='blue'>
+                        <Card.Content>
+                            <Icon name="hand rock outline" />
+                            <Card.Description>Add a feeling for another day</Card.Description>
+                        </Card.Content>
+                    </Card> */}
                 </Card.Group>
             )
         } else {
