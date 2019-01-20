3e9584d7695589604cea41eb2acb0803761bed50
no-relation <eddiec76@gmail.com>
2019-01-19 22:34:15 -0600

    gridding and carding

--- src/views/components/daysForm.js
+++ src/views/components/daysForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, Grid } from 'semantic-ui-react';
 import { Calendar } from 'react-calendar'
 
 export class DaysForm extends Component {
@@ -21,18 +21,23 @@ export class DaysForm extends Component {
     render() {
         return (
             <Form onSubmit={this.handleOnSubmit} >
-                <Form.Group widths='equal'>
-                    <Form.Input readOnly fluid label="Start Date" value={this.state.dates[0].toDateString()} placeholder="start date" width={4} />
-                    <Form.Input readOnly fluid label="End Date" value={this.state.dates[1].toDateString()} placeholder="start date"/>
-                </Form.Group>
                 <Form.Button color='blue'>Submit</Form.Button>
-                <Calendar 
-                    minDate={new Date("2013-01-01")}
-                    selectRange={true}
-                    onChange={this.handleOnChangeCalendar}
-                    value={this.state.dates}
-                    calendarType="US"
-                />
+                <Grid>
+                    <Grid.Column width={8}>
+                        <Calendar 
+                            minDate={new Date("2013-01-01")}
+                            selectRange={true}
+                            onChange={this.handleOnChangeCalendar}
+                            value={this.state.dates}
+                            calendarType="US"
+                        />
+                    </Grid.Column>
+
+                    <Form.Group widths='equal'>
+                        <Form.Input readOnly fluid label="Start Date" value={this.state.dates[0].toDateString()} placeholder="start date" width={4} />
+                        <Form.Input readOnly fluid label="End Date" value={this.state.dates[1].toDateString()} placeholder="start date"/>
+                    </Form.Group>
+                </Grid>
             </Form>
         );
     }
