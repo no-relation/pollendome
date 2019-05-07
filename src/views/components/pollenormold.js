8d8650464444985d6bf8aaaf20f2f4f421873e06
no-relation <eddiec76@gmail.com>
2019-05-06 23:10:23 -0500

    trying to connect pollenOrMold to redux store

--- src/views/components/pollenormold.js
+++ src/views/components/pollenormold.js
@@ -1,11 +1,20 @@
 // TODO: programmatically derive pollen or mold names from backend and database
-const pollenOrMold = (name) => {
+// how do I connect this function to the redux store?
+
+import { days_actions } from '../../state/actions/days_actions';
+import { connect } from 'react-redux';
+
+const _pollenOrMold = (name) => {
    
     const mold_names = ["acrodictys", "agrocybe", "algae", "alternaria", "arthimium", "ascomycetes", "asperisporium", "basidiomycetes", "beltrania", "botrytis", "cercospora", "cladosporium", "curvularia", "d_conidia_hyphae", "dendryphiella", "drechslera_helmintho", "dichotomophthora", "diplococcum", "epicoccum", "fusariella", "ganoderma", "helicomina", "microsporum", "misc_fungus_hyaline", "monodictys", "myxomycete_smut", "nigrospora", "penicillium_aspergillus", "periconia", "pestalotiopsis", "pithomyces", "powdery_mildew", "pseudocercospora", "puccinia", "rust", "spegazinia", "stemphyllium", "tetrapola", "tilletia", "torula"]
     
     const tree_pollen_names = ["alder", "ash", "ashe_juniper___bald_cypress", "black_gum", "black_walnut", "bushes", "birch", "cotton_wood", "dogwood", "elm", "glandular_mesquite", "hackberry", "hickory", "mulberry", "maple", "osage_orange", "oak", "sycamore", "pine", "privet", "sweet_gum", "gingko_biloba", "walnut", "willow", "willow", "other_tree", "cedar", "hazelnut", "plum_grannet", "walnutjuglans", "aster", "nettle", "magnolia", "wild_carrot", ]
 
     const weed_pollen_names = ["amaranth", "burweed___marshelder", "cattail", "dog_fennel", "lambs_quarters", "partridge_pea", "pigweed", "plantago", "ragweed", "rumex", "sagebrush", "saltbrush", "sedge", "sneezeweed", "other_weed"]
+
+    // if (!this.props.specieses) {
+    //     this.props.getSpecies()
+    // }
 
     if (mold_names.includes(name)) {
         return 'mold'
@@ -16,4 +25,10 @@ const pollenOrMold = (name) => {
     }
 }
 
+// const MapStateToProps = (state) => ({
+//     specieses:  state.specieses
+
+// })
+
 export { pollenOrMold }
+// = connect(MapStateToProps, days_actions)(_pollenOrMold)
